7a3a30ec0eed246e9e4b6258d777da29960860cd
Erik Abair <erik.abair@gmail.com>
2024-08-17 14:09:50 -0700

    Adds button to launch accessibility settings.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,12 +23,21 @@ android {
       proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
     }
   }
+
+  buildFeatures {
+    compose = true
+  }
+
+  composeOptions {
+    kotlinCompilerExtensionVersion = "1.5.15"
+  }
+
   compileOptions {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
   }
   kotlinOptions {
-    jvmTarget = "1.8"
+    jvmTarget = "17"
   }
 }
 
@@ -40,4 +49,11 @@ dependencies {
   testImplementation(libs.junit)
   androidTestImplementation(libs.androidx.junit)
   androidTestImplementation(libs.androidx.espresso.core)
+
+  // Jetpack Compose
+  implementation(platform(libs.androidx.compose.bom))
+  implementation(libs.androidx.activity.compose)
+  implementation(libs.androidx.ui)
+
+  implementation(libs.androidx.material3)
 }
--- app/src/main/java/work/bearbrains/joymouse/MainActivity.kt
+++ app/src/main/java/work/bearbrains/joymouse/MainActivity.kt
@@ -1,9 +1,36 @@
 package work.bearbrains.joymouse
 
 import android.content.Intent
+import android.os.Bundle
+import android.provider.Settings
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 
 class MainActivity : ComponentActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContent {
+      JoyMouseTheme {
+        Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+          Button(
+              onClick = {
+                val intent = Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS)
+                startActivity(intent)
+              }) {
+                Text(stringResource(id = R.string.button_open_accessibility_settings))
+              }
+        }
+      }
+    }
+  }
 
   override fun onResume() {
     super.onResume()
