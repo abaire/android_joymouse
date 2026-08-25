872bbf72a0fd4da17eef102b738d5bb3a15b0425
Erik Abair <erik.abair@gmail.com>
2026-08-24 22:16:37 -0700

    Removes unusued overlay enable code.

--- app/src/main/java/work/bearbrains/joymouse/MainActivity.kt
+++ app/src/main/java/work/bearbrains/joymouse/MainActivity.kt
@@ -1,24 +1,18 @@
 package work.bearbrains.joymouse
 
-import android.app.Activity
 import android.content.Intent
 import android.content.pm.ApplicationInfo
-import android.net.Uri
 import android.os.Bundle
 import android.os.StrictMode
 import android.provider.Settings
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
 import work.bearbrains.joymouse.ui.JoyMouseTheme
 import work.bearbrains.joymouse.ui.MainScreen
 
 class MainActivity : ComponentActivity() {
-
-  private val overlayEnabledState = mutableStateOf(false)
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
@@ -33,30 +27,13 @@ class MainActivity : ComponentActivity() {
       )
     }
 
-    overlayEnabledState.value = Settings.canDrawOverlays(this)
-    val manageOverlayActivityResult =
-      registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-        if (result.resultCode == Activity.RESULT_OK) {
-          overlayEnabledState.value = Settings.canDrawOverlays(this)
-        }
-      }
-
     setContent {
       JoyMouseTheme {
         MainScreen(
-          overlayEnabledState.value,
           modifier = Modifier.fillMaxSize(),
           onLaunchAccessibilitySettings = {
             val intent = Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS)
             startActivity(intent)
-          },
-          onEnableOverlay = {
-            val intent =
-              Intent(
-                Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
-                Uri.parse("package:$packageName"),
-              )
-            manageOverlayActivityResult.launch(intent)
           },
         )
       }
